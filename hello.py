7f64817d793372d1af2293d27db9ca12c0b48831
nicolas13sochi <nikolay.grishchenko@gmail.com>
2021-01-26 20:59:54 +0300

    Created password generator

--- hello.py
+++ hello.py
@@ -1,2 +1,13 @@
 # hello.py
+import random
+
 print("Hello World!")
+abcd = "qwertyuiopasdfghjklzxcvbnm1234567890-=+_)(*&^%$#@!QWERTYUIOPASDFGHJKLZXCVBNM"
+
+def gen_pas(n=12):
+    r = ""
+    for i in range(n):
+        r += random.choice(abcd)
+    print(r)
+
+gen_pas()
